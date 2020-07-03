ab8935c757637adfb105621e433dc0f5951eea1a
Alexandre <kavalerskialexandre@gmail.com>
2020-07-03 16:45:08 -0300

    Update node functions with tests

--- src/functions/node.ts
+++ src/functions/node.ts
@@ -2,14 +2,39 @@ import { operations } from './../utils/operations';
 import { NodeInfo } from "../classes/Node"
 import { State } from '../utils/state';
 import { calcHeuristicValue } from './heuristic';
+import { applyOperation } from './operations';
 
 function generateNodeList(node: NodeInfo, goalState: State, gValue: number){
-    const s: State = [[4,5,8], [null,1,6], [7, 2, 3]];
-    const n = new NodeInfo(1, operations.none, s);
-    return [n];
+    let childrenNodes: NodeInfo[] = [];
+    const childUp = generateAndTest(operations.up, node, goalState, gValue);
+    if (childUp){
+        childrenNodes.push(childUp);
+    }
+    const childRight = generateAndTest(operations.right, node, goalState, gValue);
+    if (childRight){
+        childrenNodes.push(childRight);
+    }
+    const childDown = generateAndTest(operations.down, node, goalState, gValue);
+    if (childDown){
+        childrenNodes.push(childDown);
+    }
+    const childLeft = generateAndTest(operations.left, node, goalState, gValue);
+    if (childLeft){
+        childrenNodes.push(childLeft);
+    }
+       
+    return childrenNodes.sort((a, b) => (a.evaluationFunctionValue < b.evaluationFunctionValue) ? -1 : 1);
 }
 
-function generateNode(state: State, op: operations, goalState: State, gValue: number, previousNode?: NodeInfo){
+function generateAndTest(op: operations, node: NodeInfo, goalState: State, gValue: number): NodeInfo | null{
+    const newState = applyOperation(node.state, op);
+    if(newState){
+        return generateNode(newState, op, goalState, (gValue+1), node);
+    }
+    return null;
+}
+
+function generateNode(state: State, op: operations, goalState: State, gValue: number, previousNode?: NodeInfo): NodeInfo{
     const heuristicValue = calcHeuristicValue(state, goalState, gValue);
     return new NodeInfo(heuristicValue, op, state, previousNode);
 }
--- tests/node.spec.ts
+++ tests/node.spec.ts
@@ -59,3 +59,50 @@ describe('Testing returns of generateNode function', () => {
         expect(generateNode(state, operations.up, finalState, gValue, mockedPreviousNode)).to.eql(expectedNode);
     });
 });
+
+
+describe('Testing returns of generateNodeList function', () => {  
+    
+    it('should return this NodeInfo array when call generateNodeList function', () => {
+        const state: State = [[4,5,8], [null,1,6], [7, 2, 3]];
+        const goalState: State = [[1,2,3], [4,5,6], [7, 8, null]];
+        const rootNode = new NodeInfo(14, operations.none, state);
+        const gValue = 0;
+
+        const stateChild1: State = [[null,5,8], [4,1,6], [7, 2, 3]];
+        const expectedChild1 = new NodeInfo(15, operations.up, stateChild1, rootNode);
+        
+        const stateChild2: State = [[4,5,8], [1,null,6], [7, 2, 3]];
+        const expectedChild2 = new NodeInfo(13, operations.right, stateChild2, rootNode);
+        
+        const stateChild3: State = [[4,5,8], [7,1,6], [null, 2, 3]];
+        const expectedChild3 = new NodeInfo(15, operations.down, stateChild3, rootNode);
+
+        const childrenList = [expectedChild2, expectedChild1 , expectedChild3];
+
+        expect(generateNodeList(rootNode, goalState, gValue)).to.eql(childrenList);
+    });
+
+    it('should return this NodeInfo array when call generateNodeList function', () => {
+        const state: State = [[4,5,8], [1,null,6], [7, 2, 3]];
+        const goalState: State = [[1,2,3], [4,5,6], [7, 8, null]];
+        const rootNode = new NodeInfo(13, operations.right, state);
+        const gValue = 1;
+
+        const stateChild1: State = [[4,null,8], [1,5,6], [7, 2, 3]];
+        const expectedChild1 = new NodeInfo(14, operations.up, stateChild1, rootNode);
+        
+        const stateChild2: State = [[4,5,8], [1,6,null], [7, 2, 3]];
+        const expectedChild2 = new NodeInfo(14, operations.right, stateChild2, rootNode);
+        
+        const stateChild3: State = [[4,5,8], [1,2,6], [7, null, 3]];
+        const expectedChild3 = new NodeInfo(12, operations.down, stateChild3, rootNode);
+        
+        const stateChild4: State = [[4,5,8], [null,1,6], [7, 2, 3]];
+        const expectedChild4 = new NodeInfo(16, operations.left, stateChild4, rootNode);
+
+        const childrenList = [expectedChild3, expectedChild1, expectedChild2, expectedChild4];
+
+        expect(generateNodeList(rootNode, goalState, gValue)).to.eql(childrenList);
+    });
+});
