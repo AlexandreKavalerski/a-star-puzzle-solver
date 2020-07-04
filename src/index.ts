5d437eede34bbfb35dcda8ca75e9f25d72c8e09f
Alexandre <kavalerskialexandre@gmail.com>
2020-07-04 19:33:13 -0300

    Update index (solved rangeerror)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { NodeInfo } from "./classes/Node";
 import { generateNodeList, generateNode, readNode } from './functions/node';
 import { areEqual, includes } from './functions/state';
 
-const initialState: (number | null)[][] = [[4,5,8], [5,4,6], [7, null, 8]];
+const initialState: (number | null)[][] = [[4,5,8], [null,1,6], [7, 2, 3]];
 const finalState: (number | null)[][] = [[1,2,3], [4,5,6], [7, 8, null]];
 
 const firstNode = generateNode(initialState, operations.none, finalState, 0);
@@ -13,8 +13,39 @@ let expandedStates: [] = [];
 
 
 
-const finalNode = run(finalState, frontier, expandedStates);
-readNode(finalNode);
+const finalNode = runWhile(finalState, frontier, expandedStates);
+if(finalNode){
+    readNode(finalNode);
+}
+
+function runWhile(goalState: (number | null)[][], frontier: NodeInfo[], expandedStates: (number | null)[][][]){
+    let actualNode = frontier.shift();
+    if(actualNode){
+        while(!areEqual(actualNode.state, goalState)){
+            const children = generateNodeList(actualNode, goalState);
+            for (let c of children){
+                if(!includes(c.state, expandedStates)){//Only add to frontier states not openned yet
+                    frontier.push(c);
+                    expandedStates.push(actualNode.state);
+                }
+            }
+
+            frontier.sort((a, b) => (a.evaluationFunctionValue.f < b.evaluationFunctionValue.f) ? -1 : ((a.evaluationFunctionValue.f === b.evaluationFunctionValue.f) ? ((a.evaluationFunctionValue.g > b.evaluationFunctionValue.g) ? -1 : 1): 1)); // Order frontier according to heuristic value of nodes
+            
+            const aux = frontier.shift();
+            if(aux){
+                actualNode = aux;
+            }else{
+                throw new Error('Empty frontier');
+            }
+            if(areEqual(actualNode.state, goalState)){
+                return actualNode;
+            }
+        }
+        return actualNode;
+    }
+}
+
 
 function run(goalState: (number | null)[][], frontier: NodeInfo[], expandedStates: (number | null)[][][]): NodeInfo{
     const actualNode = frontier.shift();
