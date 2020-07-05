59f6052f1c16aedec111300a8cccbe377f7672af
Alexandre <kavalerskialexandre@gmail.com>
2020-07-04 22:27:58 -0300

    Refactor frontier sort (mijor f and minor h)

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import { NodeInfo } from "./classes/Node";
 import { generateNodeList, generateNode, readNode } from './functions/node';
 import { areEqual, includes } from './functions/state';
 
-const initialState: State = [[1,2,3], [4,5,6], [7, null, 8]];
+// const initialState: State = [[4,5,8], [null,1,6], [7, 2, 3]];
+const initialState: State = [[1,2,3], [5,4,6], [7, null, 8]];
 const finalState: State = [[1,2,3], [4,5,6], [7, 8, null]];
 
 const firstNode = generateNode(initialState, operations.none, finalState, 0);
@@ -29,7 +30,7 @@ function run(goalState: State, frontier: NodeInfo[], expandedStates: State[]){
                 }
             }
 
-            frontier.sort((a, b) => (a.evaluationFunctionValue.f < b.evaluationFunctionValue.f) ? -1 : ((a.evaluationFunctionValue.f === b.evaluationFunctionValue.f) ? ((a.evaluationFunctionValue.g > b.evaluationFunctionValue.g) ? -1 : 1): 1)); // Order frontier according to heuristic value of nodes
+            frontier.sort((a, b) => (a.evaluationFunctionValue.f < b.evaluationFunctionValue.f) ? -1 : ((a.evaluationFunctionValue.f === b.evaluationFunctionValue.f) ? ((a.evaluationFunctionValue.h < b.evaluationFunctionValue.h) ? -1 : 1): 1)); // Order frontier according to heuristic value of nodes
 
             const aux = frontier.shift();
             if(aux){
