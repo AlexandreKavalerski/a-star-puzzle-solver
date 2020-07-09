f4dc22c72bab13dd3ebca24dc797eec13b57bb14
Alexandre <kavalerskialexandre@gmail.com>
2020-07-09 20:11:10 -0300

    Update dist folder (corrected case sense of utils folder)

---
+++ dist/utils/operations.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.operations = void 0;
+var operations;
+(function (operations) {
+    operations["up"] = "UP_OPERATION";
+    operations["right"] = "RIGHT_OPERATION";
+    operations["down"] = "DOWN_OPERATION";
+    operations["left"] = "LEFT_OPERATION";
+    operations["none"] = "NONE";
+})(operations = exports.operations || (exports.operations = {}));
+//# sourceMappingURL=operations.js.map
